46dda2fcb2440a0591f732bffd9f71ec3c4c76eb
mijosan <mijosan@naver.com>
2020-04-11 06:28:12 +0900

    2588 번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,9 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		//StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		
+		int c = b;
+	    System.out.println(a*(b%10));
+	    
+	    b = b/10;
+	    System.out.println(a*(b%10));
+	    
+	    b = b/10;
+	    System.out.println(a*(b%10));
+	    
+	    System.out.println(a*c);
 	}
 
 }
