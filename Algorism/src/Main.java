c9b0569ae72b0e9783a934ca051a2d92619d0068
mijosan <mijosan@naver.com>
2020-04-11 11:26:05 +0900

    5543번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,29 +1,45 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	StringTokenizer st;
-		
-		int count = Integer.parseInt(br.readLine());
-		
-		for(int i=0;i<count;i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			bw.write(a+b+"\n");
+		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		//StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
+		int burger1 = Integer.parseInt(br.readLine());
+		int burger2 = Integer.parseInt(br.readLine());
+		int burger3 = Integer.parseInt(br.readLine());
+		int beverage1 = Integer.parseInt(br.readLine());
+		int beverage2 = Integer.parseInt(br.readLine());
+		
+		int min1;
+		int min2;
+		
+		int result;
+		if(burger1 > burger2) {
+			min1 = burger2;
+			if(burger2>burger3) {
+				min1 = burger3;
+			}
+		}else {
+			min1 = burger1;
+			if(burger1 > burger3) {
+				min1 = burger3;
+			}
 		}
-		bw.flush();
+		
+		if(beverage1 > beverage2) {
+			min2 = beverage2;
+		}else {
+			min2 = beverage1;
+		}
+		
+		result = min1 + min2 - 50;
+		
+		System.out.println(result);
 	}
-
 }
