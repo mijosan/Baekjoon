8cf08b131738fd5bd6773d4bba92e49f522997a0
mijosan <mijosan@naver.com>
2020-04-11 12:23:05 +0900

    2523번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -9,37 +9,18 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		//StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
 
-		int burger1 = Integer.parseInt(br.readLine());
-		int burger2 = Integer.parseInt(br.readLine());
-		int burger3 = Integer.parseInt(br.readLine());
-		int beverage1 = Integer.parseInt(br.readLine());
-		int beverage2 = Integer.parseInt(br.readLine());
 		
-		int min1;
-		int min2;
-		
-		int result;
-		if(burger1 > burger2) {
-			min1 = burger2;
-			if(burger2>burger3) {
-				min1 = burger3;
-			}
-		}else {
-			min1 = burger1;
-			if(burger1 > burger3) {
-				min1 = burger3;
-			}
+		for(int i=0;i<n;i++) {
+			sb.append("*");
+			System.out.println(sb);
 		}
 		
-		if(beverage1 > beverage2) {
-			min2 = beverage2;
-		}else {
-			min2 = beverage1;
+		for(int i=0;i<n;i++) {
+			sb.deleteCharAt(sb.length()-1);
+			System.out.println(sb);
 		}
-		
-		result = min1 + min2 - 50;
-		
-		System.out.println(result);
 	}
 }
