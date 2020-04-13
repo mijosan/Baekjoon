e670f4811494fa0570ff11a697c60655f4d15e83
mijosan <mijosan@naver.com>
2020-04-13 16:01:20 +0900

    10828번 스택

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -5,31 +5,84 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+	
+	
+	
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		//StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		//StringBuilder sb = new StringBuilder();
 		
-		int n = Integer.parseInt(br.readLine());
-		int count = 0; // 그룹 아닌 갯수
 		
-		for(int i=0;i<n;i++) {	
-			boolean[] bl = new boolean[27];
-			String s = br.readLine();
+		int n = Integer.parseInt(br.readLine());
+		Stack stack = new Stack(n);
+		
+		for(int i=0;i<n;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
 			
-			for(int j=0;j<s.length();j++) {
-
-				if(bl[s.charAt(j)-97] == true) {
-					if(j !=0 && s.charAt(j-1) != s.charAt(j)) {
-						count = count + 1;			
-						break;
-					}
-				}else {
-					bl[s.charAt(j)-97] = true;
-				}		
+			String cmd = st.nextToken();
+			
+			if(cmd.equals("push")) {
+				stack.push(Integer.parseInt(st.nextToken()));
+			}else if(cmd.equals("pop")) {
+				stack.pop();
+			}else if(cmd.equals("size")) {
+				stack.size();
+			}else if(cmd.equals("empty")) {
+				stack.empty();
+			}else if(cmd.equals("top")) {
+				stack.top();
 			}
 		}
-		System.out.println(n-count);
+	}
+	
+}
+
+class Stack{
+	
+	int[] stack;
+	int top;
+	
+	
+	Stack(int n){
+		stack = new int[n];
+	}
+	
+	
+	void push(int x) {
+		stack[top] = x;
+		top++;
+	}
+	
+	void pop() {
+		if(top == 0) {
+			System.out.println("-1");
+		}else {
+			--top;
+			System.out.println(stack[top]);
+			stack[top] = 0;
+		}
+	}
+	
+	void size() {
+		System.out.println(top);
+	}
+	
+	void empty() {
+		if(top == 0) {
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+	}
+	
+	void top() {
+		if(top == 0) {
+			System.out.println("-1");
+		}else {
+			System.out.println(stack[top-1]);
+		}
 	}
 }
