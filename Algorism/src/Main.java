e9dcde3890b5a4257ee183d026717ac9e1e78c24
mijosan <mijosan@naver.com>
2020-04-13 21:49:35 +0900

    4949번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -2,45 +2,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	
-	
-	
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		//StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		//StringTokenizer st = new StringTokenizer(br.readLine(),".");
 		//StringBuilder sb = new StringBuilder();
 		
-		int n = Integer.parseInt(br.readLine());
-	
-		for(int i=0;i<n;i++) {
-			Stack<Character> stack = new Stack<Character>();
-			String s = br.readLine();
-			
-			for(int j=0;j<s.length();j++) {
-
-				if(s.charAt(j) == '(') {
-					stack.add('(');
-				}else {
-					if(stack.empty()) {
-						stack.add('x');
-						break;
-					}else {
-						stack.pop();
-					}
+		Stack<Character> stack = new Stack<Character>();
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine(),".");
+			String s = null;
+			if(st.hasMoreTokens()) {
+				s = st.nextToken();
+			}else {
+				break;
+			}
+			for(int i=0;i<s.length();i++) {
+				if(s.charAt(i) == '(' || s.charAt(i) == '[') {
+					stack.push(s.charAt(i));
+					//System.out.println(s.charAt(i) + " Çª½Ã");
+				}else if(stack.isEmpty() == false && s.charAt(i) == ')' && stack.peek() == '(') {
+					//System.out.println(stack.peek() + " ÆË");
+					stack.pop();
+				}else if(stack.isEmpty() == false && s.charAt(i) == ']' && stack.peek() == '[') {
+					//System.out.println(stack.peek() + " ÆË");
+					stack.pop();
+				}else if(s.charAt(i) == ')' || s.charAt(i) == ']'){
+					stack.push('x');
+					break;
 				}
 			}
-			if(stack.empty()) {
-				System.out.println("YES");
+			
+			if(stack.isEmpty()) {
+				System.out.println("yes");
 			}else {
-				System.out.println("NO");
+				System.out.println("no");
 			}
+
+			stack.clear();
 		}
-		
+	
 	}
 	
 }
