0b6c58e106e20510507322affc745bea14f1e265
mijosan <mijosan@naver.com>
2020-04-13 18:23:23 +0900

    9012번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
 public class Main {
 
@@ -15,74 +15,33 @@ public class Main {
 		//StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		//StringBuilder sb = new StringBuilder();
 		
-		
 		int n = Integer.parseInt(br.readLine());
-		Stack stack = new Stack(n);
-		
+	
 		for(int i=0;i<n;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			Stack<Character> stack = new Stack<Character>();
+			String s = br.readLine();
 			
-			String cmd = st.nextToken();
-			
-			if(cmd.equals("push")) {
-				stack.push(Integer.parseInt(st.nextToken()));
-			}else if(cmd.equals("pop")) {
-				stack.pop();
-			}else if(cmd.equals("size")) {
-				stack.size();
-			}else if(cmd.equals("empty")) {
-				stack.empty();
-			}else if(cmd.equals("top")) {
-				stack.top();
+			for(int j=0;j<s.length();j++) {
+
+				if(s.charAt(j) == '(') {
+					stack.add('(');
+				}else {
+					if(stack.empty()) {
+						stack.add('x');
+						break;
+					}else {
+						stack.pop();
+					}
+				}
+			}
+			if(stack.empty()) {
+				System.out.println("YES");
+			}else {
+				System.out.println("NO");
 			}
 		}
+		
 	}
 	
 }
 
-class Stack{
-	
-	int[] stack;
-	int top;
-	
-	
-	Stack(int n){
-		stack = new int[n];
-	}
-	
-	
-	void push(int x) {
-		stack[top] = x;
-		top++;
-	}
-	
-	void pop() {
-		if(top == 0) {
-			System.out.println("-1");
-		}else {
-			--top;
-			System.out.println(stack[top]);
-			stack[top] = 0;
-		}
-	}
-	
-	void size() {
-		System.out.println(top);
-	}
-	
-	void empty() {
-		if(top == 0) {
-			System.out.println("1");
-		}else {
-			System.out.println("0");
-		}
-	}
-	
-	void top() {
-		if(top == 0) {
-			System.out.println("-1");
-		}else {
-			System.out.println(stack[top-1]);
-		}
-	}
-}
