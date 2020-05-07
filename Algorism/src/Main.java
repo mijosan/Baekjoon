79d7c53d32bb9d1644db10cccd33336579cc6db3
mijosan <mijosan@naver.com>
2020-05-07 09:13:53 +0900

    완전탐색-백준-7568번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -10,35 +10,38 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		// StringTokenizer st = new StringTokenizer(br.readLine(),".");
-		// StringBuilder sb = new StringBuilder();
+		// StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
-
-		int[] arr = new int[9];
-
-		int sum = 0;
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			sum = sum + arr[i];
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[n][2];
+		for(int i=0;i<n;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			
+			int weight = Integer.parseInt(st.nextToken());
+			int height = Integer.parseInt(st.nextToken());
+			
+			arr[i][0] = weight;
+			arr[i][1] = height;
 		}
 		
-		loop:
-		for (int i = 0; i < 9; i++) {
-			for (int j = i + 1; j < 9; j++) {
-				if (sum - (arr[i] + arr[j]) == 100) {
-					arr[i] = 0;
-					arr[j] = 0;
-					break loop;
+		for(int i=0;i<n;i++) {
+			
+			int rank = 1;
+			
+			for(int j=0;j<n;j++) {
+				System.out.println(arr[i][0]+" "+ arr[j][0] +" "+ arr[i][1] +" "+ arr[j][1]);
+				if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+					rank++;
 				}
 			}
-		}
+					
+			sb.append(rank + " ").toString().trim();
 
-		Arrays.sort(arr);
-	  
-		for(int i=0;i<arr.length;i++) { 
-			if(arr[i] != 0) { 
-				System.out.println(arr[i]);
-			} 
 		}
+		
+		System.out.println(sb);
 	}
 }
