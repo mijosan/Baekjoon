3c450b30932ed96bcac3139405adf8a51a09000e
mijosan <mijosan@naver.com>
2020-05-08 15:02:41 +0900

    DP-백준-9095번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,47 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
-import java.util.StringTokenizer;
+//DP 자체는 이렇게 정의합니다. 주어진 문제를 여러 개의 부분 문제들로 나누어 푼 다음, 그 결과들로 주어진 문제를 푼다.
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		// StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		StringBuilder sb = new StringBuilder();
+		// StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		int[][] arr = new int[n][2];
-		for(int i=0;i<n;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine()," ");
-			
-			int weight = Integer.parseInt(st.nextToken());
-			int height = Integer.parseInt(st.nextToken());
-			
-			arr[i][0] = weight;
-			arr[i][1] = height;
-		}
-		
-		for(int i=0;i<n;i++) {
-			
-			int rank = 1;
-			
-			for(int j=0;j<n;j++) {
-				System.out.println(arr[i][0]+" "+ arr[j][0] +" "+ arr[i][1] +" "+ arr[j][1]);
-				if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
-					rank++;
-				}
-			}
-					
-			sb.append(rank + " ").toString().trim();
+    	
+    	int t = Integer.parseInt(br.readLine());
+    	
+    	int[] dp = new int[12];
+    	
+    	dp[1] = 1;
+    	dp[2] = 2;
+    	dp[3] = 4;
+    	
+    	for(int i=0;i<t;i++) {
+    		int n = Integer.parseInt(br.readLine());
+    		
+    		for(int j=4;j<=n;j++) {
+    			dp[j] = dp[j-1] + dp[j-2] + dp[j-3];
+    		}
+    		
+    		System.out.println(dp[n]);
+    	}
+    }
 
-		}
-		
-		System.out.println(sb);
-	}
 }
