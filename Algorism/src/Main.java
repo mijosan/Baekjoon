9f6cd539a5a6c967cabcd4c38ad8d25274aae3f0
mijosan <mijosan@naver.com>
2020-05-11 17:28:47 +0900

    dp-백준-2579번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -15,23 +15,28 @@ public class Main {
     	
     	int n = Integer.parseInt(br.readLine());
     	
-    	int[][] dp = new int[n][3];
-   	
-    	for(int i=0;i<n;i++) {
-    		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-    		for(int j=0;j<3;j++) {
-    			dp[i][j] = Integer.parseInt(st.nextToken());
-    		}
+    	int[] dp = new int[n+1];
+    	int[] map = new int[n+1];
+    	
+    	for(int i=1;i<=n;i++) {
+    		map[i] = Integer.parseInt(br.readLine());
     	}
     	
-    	for(int i=1;i<n;i++) {
-    		
-    		dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + dp[i][0];
-    		dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + dp[i][1];
-    		dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + dp[i][2];
+    	
+		dp[1] = map[1];
+		
+		if(n > 1) {
+			dp[2] = map[1]+map[2];
+			
+			if(n > 2) {
+				dp[3] = Math.max(map[1]+map[3], map[2]+map[3]);
+			}
+		}
+    	for(int i=4;i<=n;i++) {
+    		dp[i] = Math.max(dp[i-2], dp[i-3]+map[i-1]) + map[i];
     	}
     	
-    	System.out.println(Math.min(Math.min(dp[n-1][0], dp[n-1][1]),dp[n-1][2]));
+    	System.out.println(dp[n]);
     	
     		
     }
