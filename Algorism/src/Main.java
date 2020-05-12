b77fd544490368aaa733d664a440fed8b275f950
mijosan <mijosan@naver.com>
2020-05-12 18:58:37 +0900

    dp-백준-2156번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -16,18 +16,24 @@ public class Main {
     	int n = Integer.parseInt(br.readLine());
     	
     	Long[] dp = new Long[n+1];
+    	Long[] arr = new Long[n+1];
     	
-    	dp[1] = 1L;
-    	
-    	if(n >= 2) {
-    		dp[2] = 1L;
+    	for(int i=1;i<=n;i++) {
+    		arr[i] = Long.parseLong(br.readLine());
     	}
     	
-    	for(int i=3;i<=n;i++) {
-    		dp[i] = dp[i-1] + dp[i-2];
+    	if(n >= 1) dp[1] = arr[1];
+    	if(n >= 2) dp[2] = arr[1] + arr[2];	
+    	if(n >= 3) dp[3] = Math.max(Math.max(dp[2], arr[2]+arr[3]),arr[1]+arr[3]);
+    	
+    	for(int i=4;i<=n;i++) {
+    		
+    		dp[i] = Math.max(Math.max(dp[i-1], dp[i-3] + arr[i-1] + arr[i]), dp[i-2] + arr[i]); 
     	}
     	
     	System.out.println(dp[n]);
+    	
+    	
     	
     	
     	
