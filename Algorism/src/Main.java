023864da9e42c3e3abf77508238aee8b8d55c1ee
mijosan <mijosan@naver.com>
2020-05-12 17:04:40 +0900

    dp-백준-2193번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -15,44 +15,22 @@ public class Main {
     	
     	int n = Integer.parseInt(br.readLine());
     	
-    	int[][] dp = new int[n+1][n+1];
+    	Long[] dp = new Long[n+1];
     	
-    	for(int i=1;i<=n;i++) {
-    		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-    		
-    		for(int j=1;j<=i;j++) {
-    			dp[i][j] = Integer.parseInt(st.nextToken());
-    		}
-    	}
+    	dp[1] = 1L;
     	
-
     	if(n >= 2) {
-    		dp[2][1] = dp[1][1] + dp[2][1];
-    		dp[2][2] = dp[1][1] + dp[2][2];
+    		dp[2] = 1L;
     	}
     	
     	for(int i=3;i<=n;i++) {
-    		
-    		dp[i][1] = dp[i-1][1] + dp[i][1];
-    		
-    		for(int j=2;j<i;j++) {
-
-    			dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + dp[i][j];
-    			
-    		}
-    		
-    		dp[i][i] = dp[i-1][i-1] + dp[i][i];
+    		dp[i] = dp[i-1] + dp[i-2];
     	}
     	
-    	int max = Integer.MIN_VALUE;
+    	System.out.println(dp[n]);
     	
-    	for(int i=1;i<=n;i++) {
-    		if(max < dp[n][i]) {
-    			max = dp[n][i];
-    		}
-    	}
     	
-    	System.out.println(max);
+    	
     		
     }
 
