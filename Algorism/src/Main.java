e77a3136584b10b00f511b6f33b5c406dc73e4c9
mijosan <mijosan@naver.com>
2020-05-20 10:08:41 +0900

    bfs-백준-1260번 <최종버전>

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -14,98 +14,90 @@ public class Main {
 
     public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		// StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		// StringBuilder sb = new StringBuilder();
-		// int n = Integer.parseInt(br.readLine());
-    	
-    	
-    	//int n = Integer.parseInt(br.readLine());
-    	
+
     	StringTokenizer st = new StringTokenizer(br.readLine()," ");
     	
     	int vertex = Integer.parseInt(st.nextToken());
     	int edge = Integer.parseInt(st.nextToken());
     	int start = Integer.parseInt(st.nextToken());
-    	boolean[] visited = new boolean[vertex+1];
+    	boolean[] visited = new boolean[vertex + 1];
     	
-    	ArrayList<Integer>[] adjList = new ArrayList[vertex+1];
+    	ArrayList<Integer>[] adjList = new ArrayList[vertex + 1];
     	
     	for(int i=0;i<adjList.length;i++) {
     		adjList[i] = new ArrayList<Integer>();
     	}
     	
     	for(int i=0;i<edge;i++) {
-    		 st = new StringTokenizer(br.readLine()," ");
-    		 int v1 = Integer.parseInt(st.nextToken());
-    		 int v2 = Integer.parseInt(st.nextToken());
-    		 
-    		 adjList[v1].add(v2);
-    		 adjList[v2].add(v1);
+    		st = new StringTokenizer(br.readLine()," ");
+    		
+    		int v1 = Integer.parseInt(st.nextToken());
+    		int v2 = Integer.parseInt(st.nextToken());
+    		
+    		adjList[v1].add(v2);
+    		adjList[v2].add(v1);
     	}
-    	
+
     	for(int i=0;i<adjList.length;i++) {
-    		Collections.sort(adjList[i]); //오름차순
+    		Collections.sort(adjList[i]);
     	}
-    	
     	
     	dfs(adjList, visited, start);
     	
-    	System.out.println();
     	Arrays.fill(visited, false);
+    	System.out.println();
     	
-    	bfs(adjList, visited, start);
+    	bfs(adjList, visited, start);   	
     }
     
-    static void dfs(ArrayList<Integer>[] adjList, boolean[] visited, int v) {
-    	Stack<Integer> stack = new Stack<>();
-    	int n = adjList.length-1;
+    static void dfs(ArrayList<Integer>[] adjList, boolean[] visited, int vertex) {
+    	Stack<Integer> stack = new Stack<Integer>();
     	boolean flag = false;
     	
-    	stack.push(v);
-    	visited[v] = true;
-    	System.out.print(v + " ");
+    	stack.push(vertex);
+    	visited[vertex] = true;
+    	System.out.print(vertex + " ");
     	
     	while(!stack.isEmpty()) {
-    		int top = stack.peek();
-    		
-    		flag = false;
-    		
-    		for(int i : adjList[top]) {
-    			if(visited[i] == false) {
-    				stack.push(i);
-    				
-    				System.out.print(i + " ");
-    				visited[i] = true;
-    				flag = true;
-    				break;
-    			}
-    		}
-    		
-    		if(flag == false) {
-    			stack.pop();
-    		}
-    		
+	    	int top = stack.peek();
+	    	
+	    	flag = false;
+	    	
+	    	for(int i : adjList[top]) {
+	    		if(visited[i] == false) {
+		    		stack.push(i);
+		    		visited[i] = true;
+		    		System.out.print(i + " ");
+		    		
+		    		flag = true;
+		    		break;
+	    		}
+	    	}
+	    	
+	    	if(flag == false) {
+	    		stack.pop();
+	    	}
     	}
     }
     
-    static void bfs(ArrayList<Integer>[] adjList, boolean[] visited, int v) {
-        Queue<Integer> q = new LinkedList<>();
-
-        q.add(v);
-        visited[v] = true;
-
-        while(!q.isEmpty()) {
-            v = q.poll();
-            System.out.print(v + " ");
-
-            for(int vv: adjList[v]) {
-                if(!visited[vv]) {
-                    q.add(vv);
-                    visited[vv] = true;
-                }
-            }
-        }
-
-    }
+    static void bfs(ArrayList<Integer>[] adjList, boolean[] visited, int vertex) {
+    	Queue<Integer> queue = new LinkedList<Integer>();
+    	
+    	queue.add(vertex);
+    	visited[vertex] = true;
+    	
+    	while(!queue.isEmpty()) {
+    		
+    		int v = queue.poll();
+    		
+    		System.out.print(v + " ");
+    		
+	    	for(int i : adjList[v]) {
+	    		if(visited[i] == false) {
+	    			queue.add(i);
+	    			visited[i] = true;
+	    		}
+	    	}
+    	}
+    }  
 }
