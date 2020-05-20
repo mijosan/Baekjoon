980eaa9f1010f3e2cd6918b6009fbd47a73933b1
mijosan <mijosan@naver.com>
2020-05-21 08:47:29 +0900

    dfs-백준-2667번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -11,93 +11,88 @@ import java.util.StringTokenizer;
 //DP : 주어진 문제를 여러 개의 부분 문제들로 나누어 푼 다음, 그 결과들로 주어진 문제를 푼다.
 
 public class Main {
-
+	static int[][] map;
+	static int[][] visited;
+	static int[] homeCount;
+	static int complexCount;
+	static int[] x_move = {-1, 0, 1, 0};
+	static int[] y_move = {0, 1, 0, -1};
+	
     public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    	StringTokenizer st = new StringTokenizer(br.readLine()," ");
+    	int n = Integer.parseInt(br.readLine());
     	
-    	int vertex = Integer.parseInt(st.nextToken());
-    	int edge = Integer.parseInt(st.nextToken());
-    	int start = Integer.parseInt(st.nextToken());
-    	boolean[] visited = new boolean[vertex + 1];
+    	map = new int[n+2][n+2];
+    	visited = new int[n+2][n+2];
+    	homeCount = new int[n*n];
     	
-    	ArrayList<Integer>[] adjList = new ArrayList[vertex + 1];
-    	
-    	for(int i=0;i<adjList.length;i++) {
-    		adjList[i] = new ArrayList<Integer>();
-    	}
-    	
-    	for(int i=0;i<edge;i++) {
-    		st = new StringTokenizer(br.readLine()," ");
+    	for(int i=1;i<=n;i++) {
+    		String input = br.readLine();
     		
-    		int v1 = Integer.parseInt(st.nextToken());
-    		int v2 = Integer.parseInt(st.nextToken());
-    		
-    		adjList[v1].add(v2);
-    		adjList[v2].add(v1);
-    	}
-
-    	for(int i=0;i<adjList.length;i++) {
-    		Collections.sort(adjList[i]);
+    		for(int j=1;j<=n;j++) {
+    			map[i][j] = input.charAt(j-1) - '0';
+    		}
     	}
     	
-    	dfs(adjList, visited, start);
+    	for(int i=1;i<=n;i++) {
+    		for(int j=1;j<=n;j++) {
+    			if(map[i][j] == 1 && visited[i][j] == 0) {
+    				homeCount[complexCount] = 1;
+    				DFS(i, j);
+    				complexCount++;
+    			}
+    		}
+    	}
     	
-    	Arrays.fill(visited, false);
-    	System.out.println();
+    	System.out.println(complexCount);
     	
-    	bfs(adjList, visited, start);   	
+    	Arrays.sort(homeCount,0,complexCount);
+    	for(int i=0;i<complexCount;i++) {
+    		System.out.println(homeCount[i]);
+    	}
     }
     
-    static void dfs(ArrayList<Integer>[] adjList, boolean[] visited, int vertex) {
-    	Stack<Integer> stack = new Stack<Integer>();
-    	boolean flag = false;
+    static void DFS(int i, int j) {
+    	Stack<Node> stack = new Stack<Node>();
     	
-    	stack.push(vertex);
-    	visited[vertex] = true;
-    	System.out.print(vertex + " ");
+    	stack.add(new Node(i,j));
+    	visited[i][j] = 1;
     	
     	while(!stack.isEmpty()) {
-	    	int top = stack.peek();
-	    	
-	    	flag = false;
-	    	
-	    	for(int i : adjList[top]) {
-	    		if(visited[i] == false) {
-		    		stack.push(i);
-		    		visited[i] = true;
-		    		System.out.print(i + " ");
-		    		
-		    		flag = true;
-		    		break;
-	    		}
-	    	}
-	    	
-	    	if(flag == false) {
-	    		stack.pop();
-	    	}
-    	}
-    }
-    
-    static void bfs(ArrayList<Integer>[] adjList, boolean[] visited, int vertex) {
-    	Queue<Integer> queue = new LinkedList<Integer>();
-    	
-    	queue.add(vertex);
-    	visited[vertex] = true;
-    	
-    	while(!queue.isEmpty()) {
+    		Node node = stack.peek();
+    		boolean flag = false;
     		
-    		int v = queue.poll();
+    		for(int k=0;k<4;k++) {
+    			int x = node.x + x_move[k];
+    			int y = node.y + y_move[k];
+    			
+    			if(map[x][y] == 0 || visited[x][y] == 1) {
+    				continue;
+    			}
+    			
+    			stack.add(new Node(x, y));
+    			visited[x][y] = 1;
+    			homeCount[complexCount] += 1;
+    			flag = true;
+    			
+    			break;
+    		}
     		
-    		System.out.print(v + " ");
+    		if(flag == false) {
+    			stack.pop();
+    		}
     		
-	    	for(int i : adjList[v]) {
-	    		if(visited[i] == false) {
-	    			queue.add(i);
-	    			visited[i] = true;
-	    		}
-	    	}
     	}
     }  
+}
+
+class Node{
+	int x;
+	int y;
+	
+	Node(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
 }
