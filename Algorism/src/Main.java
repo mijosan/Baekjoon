2e3b4d32e1e2e4d1afbaf00094b0ee02359e9c2d
mijosan <mijosan@naver.com>
2020-05-22 21:33:10 +0900

    dfs-백준-11724번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,103 +1,90 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
-//DP : 주어진 문제를 여러 개의 부분 문제들로 나누어 푼 다음, 그 결과들로 주어진 문제를 푼다.
 
 public class Main {
-	static int[] x_move = {-1, 0, 1, 0};
-	static int[] y_move = {0, 1, 0, -1};
 	
     public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		// StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		// StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
     	
-    	int t = Integer.parseInt(br.readLine());
+    	StringTokenizer st = new StringTokenizer(br.readLine()," ");
     	
-    	for(int i=0;i<t;i++) {
-    		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-    		
-    		int m = Integer.parseInt(st.nextToken()); //가로 길이
-    		int n = Integer.parseInt(st.nextToken()); //세로 길이
-    		int k = Integer.parseInt(st.nextToken()); //배추 위치 개수
-    		int[][] map = new int[m+2][n+2];
-    		int[][] visited = new int[m+2][n+2];
-    		int count = 0;
-    		
-    		for(int j=0;j<k;j++) {
-    			st = new StringTokenizer(br.readLine()," ");
-    			
-    			int x = Integer.parseInt(st.nextToken());
-    			int y = Integer.parseInt(st.nextToken());
-
-    			
-    			map[x+1][y+1] = 1;					
-    		}
-    		
-    		for(int l=1;l<=m;l++) { 
-				for(int o=1;o<=n;o++) {
-					if(map[l][o] == 1 && visited[l][o] == 0) {
-						dfs(map, visited, l, o);
-						count++;
-					}
-				} 
-			}	
-    		
-    		System.out.println(count);
+    	int vertex = Integer.parseInt(st.nextToken());
+    	int edge = Integer.parseInt(st.nextToken());
+    	ArrayList<Integer>[] adjList = new ArrayList[vertex + 1];
+    	int[] visited = new int[vertex + 1];
+    	int count = 0;
+    	
+    	for(int i=0;i<adjList.length;i++) {
+    		adjList[i] = new ArrayList<Integer>();
     	}
-   
+    	
+    	for(int i=0;i<edge;i++) {
+    		st = new StringTokenizer(br.readLine()," ");
+    		
+    		int u = Integer.parseInt(st.nextToken());
+    		int v = Integer.parseInt(st.nextToken());
+    		
+    		adjList[u].add(v);
+    		adjList[v].add(u);
+
+    	}
+    	
+    	for(int i=1;i<=vertex;i++) {
+    		int size = adjList[i].size();
+    		
+    		for(int j=0;j<size;j++) {
+    			int start = adjList[i].get(j);
+    			
+    			if(visited[start] == 0) {
+    				bfs(adjList, visited, start);
+    				count++;
+    			}
+    		}
+    	}
+    	
+    	for(int i=1;i<visited.length;i++) {
+    		if(visited[i] == 0) {
+    			count++;
+    		}
+    	}
+    	
+    	System.out.println(count);
     }
     
-    static void dfs(int[][] map, int[][] visited, int l, int o) {
-    	Stack<Node> stack = new Stack<Node>();
+    static void bfs(ArrayList<Integer>[] adjList, int[] visited, int start) {
+    	Stack<Integer> stack = new Stack<Integer>();
     	
-    	stack.add(new Node(l,o));
-    	visited[l][o] = 1;
+    	stack.push(start);
+    	visited[start] = 1;
     	
     	while(!stack.isEmpty()) {
-    		Node node = stack.peek();
     		boolean flag = false;
+    		int top = stack.peek();
     		
-    		for(int i=0;i<4;i++) {
-    			int x = node.x + x_move[i];
-    			int y = node.y + y_move[i];
-    			
-    			if(map[x][y] == 0 || visited[x][y] == 1) {
-    				continue;
+    		for(int i : adjList[top]) {
+    			if(visited[i] == 0) {
+    				stack.push(i);
+    				visited[i] = 1;
+    				
+    				flag = true;
+    				
+    				break;
     			}
-    			
-    			stack.add(new Node(x, y));
-    			visited[x][y] = 1;
-    			flag = true;
-    			
-    			break;
     		}
     		
     		if(flag == false) {
     			stack.pop();
     		}
-    		
-    		
-    	}
+    	} 	
     }
-    
-}
-
-class Node{
-	int x;
-	int y;
-	
-	Node(int x, int y){
-		this.x = x;
-		this.y = y;
-	}
 }
