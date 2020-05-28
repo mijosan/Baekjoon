88f0bd4346efb1068a6f850cfa2a6c5cdd9565a8
mijosan <mijosan@naver.com>
2020-05-28 11:55:29 +0900

    bfs-백준-1389번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -8,11 +10,12 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int n;
-	static int start;
-	static int target;
+	static int m;
 	static ArrayList<Integer>[] adjList;
 	static int[] visited;
 	static int[] distance;
+	static int min = Integer.MAX_VALUE;
+	static int[] count;
 	
     public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,63 +24,63 @@ public class Main {
 		// StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
     	
-    	n = Integer.parseInt(br.readLine());
-    	
     	StringTokenizer st = new StringTokenizer(br.readLine()," ");
     	
-    	start = Integer.parseInt(st.nextToken());
-    	target = Integer.parseInt(st.nextToken());
+    	n = Integer.parseInt(st.nextToken());
+    	m = Integer.parseInt(st.nextToken());
+    	count = new int[n + 1];
     	
     	adjList = new ArrayList[n + 1];
     	for(int i=0;i<adjList.length;i++) {
-    		adjList[i] = new ArrayList<Integer>();
+    		adjList[i] = new ArrayList();
     	}
-
-    	visited = new int[n + 1];
-    	distance = new int[n + 1];
-    	
-    	int m = Integer.parseInt(br.readLine());
     	
     	for(int i=0;i<m;i++) {
     		st = new StringTokenizer(br.readLine()," ");
     		
-    		int x = Integer.parseInt(st.nextToken());
-    		int y = Integer.parseInt(st.nextToken());
+    		int vertex1 = Integer.parseInt(st.nextToken());
+    		int vertex2 = Integer.parseInt(st.nextToken());
     		
-    		adjList[x].add(y);
-    		adjList[y].add(x);
+    		adjList[vertex1].add(vertex2);
+    		adjList[vertex2].add(vertex1);
     	}
     	
     	bfs();
+    	
     }
     
-    static void bfs() {
-    	Queue<Integer> queue = new LinkedList<Integer>();
-    	queue.add(start);
-    	visited[start] = 1;
-    	
-    	loop:
-    	while(!queue.isEmpty()) {
-    		int vertex = queue.poll();
+    static void bfs() { 	
+    	for(int i=1;i<=n;i++) {
+    		Queue<Integer> queue = new LinkedList<Integer>();
+    		visited = new int[n + 1];
+    		distance = new int[n + 1];
+
     		
-    		for(int i : adjList[vertex]) {
-    			if(visited[i] == 0) {
-    				queue.add(i);
-    				visited[i] = 1;
-    				distance[i] = distance[vertex] + 1;
-    				
-    				if(i == target) {
-    					break loop;
-    				}
-    			}
+        	queue.add(i);
+        	visited[i] = 1;
+        	
+        	while(!queue.isEmpty()) {
+        		int vertex = queue.poll();
+        		
+        		for(int j : adjList[vertex]) {
+        			if(visited[j] == 0) {
+        				queue.add(j);
+        				visited[j] = 1;
+        				distance[j] = distance[vertex] + 1;
+        				count[i] = count[i] + distance[j];
+        			}
+        		}
+        	}
+    	}
+    	
+    	int temp = 0;
+    	
+    	for(int i=1;i<count.length;i++) {
+    		if(min > count[i]) {
+    			min = count[i];
+    			temp = i;
     		}
     	}
-    	
-    	if(visited[target] == 0) {
-    		System.out.println(-1);
-    	}else {
-    		System.out.println(distance[target]);
-    	}
+    	System.out.println(temp);
     }
-  
 }
