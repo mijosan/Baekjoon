c81994d34c41e75944bf31af55a89a15ef3c16b1
mijosan <mijosan@naver.com>
2020-05-30 05:27:40 +0900

    구현-백준-2839번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -18,69 +18,20 @@ public class Main {
 		// StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
     	
-    	StringTokenizer st = new StringTokenizer(br.readLine()," ");
+    	int n = Integer.parseInt(br.readLine());
+    	int count = 0;
     	
-    	int n = Integer.parseInt(st.nextToken());
-    	int m = Integer.parseInt(st.nextToken());
-    	
-    	int[] count = new int[n + 1];
-    	ArrayList<Integer>[] adjList = new ArrayList[n + 1];
-    	
-    	for(int i=0;i<adjList.length;i++) {
-    		adjList[i] = new ArrayList<Integer>();
+    	while(n % 5 != 0 && n > 0) {
+    		n = n - 3;
+    		count++;
     	}
     	
-    	for(int i=0;i<m;i++) {
-    		st = new StringTokenizer(br.readLine()," ");
-    		
-    		int a = Integer.parseInt(st.nextToken());
-    		int b = Integer.parseInt(st.nextToken());
-    		
-    		adjList[b].add(a);
+    	if(n < 0) {
+    		System.out.println(-1);
+    	}else {
+    		System.out.println(count + n/5);
     	}
-    	
-    	bfs(n, adjList, count);
-    	
     }
-    
-    static void bfs(int n, ArrayList<Integer>[] adjList, int[] count) {
-    	for(int i=1;i<=n;i++) {
-    		Queue<Integer> queue = new LinkedList<Integer>();
-    		int[] visited = new int[n + 1];
-    		
-    		queue.add(i);
-    		visited[i] = 1;
-    		
-    		while(!queue.isEmpty()) {
-    			int v = queue.poll();
-    			
-    			for(int j : adjList[v]) {
-    				if(visited[j] == 0) {
-    					queue.add(j);
-    					visited[j] = 1;
-    					count[i] = count[i] + 1;
-    				}
-    			}
-    		}
-    		
-    	}
-    	int max = Integer.MIN_VALUE;
-    	
-    	for(int i=1;i<=n;i++) {
-    		if(max <= count[i]) {
-    			max = count[i];
-    		}
-    	}
-    	
-    	StringBuilder sb = new StringBuilder();
-    	
-    	for(int i=1;i<=n;i++) {
-    		if(count[i] == max) {
-    			sb.append(i + " ");
-    		}
-    	}
-    	System.out.println(sb.toString().trim());
-    	
-    }
+
 
 }
