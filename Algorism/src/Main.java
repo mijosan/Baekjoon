d1c6cb8d2c1bb66be22ea4c1fefedc0111d8e38f
mijosan <mijosan@naver.com>
2020-05-30 14:09:58 +0900

    구현-백준-5597번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -15,43 +15,21 @@ public class Main {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		// StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		StringBuilder sb = new StringBuilder();
+		// StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
     	
-    	char[] a = new char[15];
-    	char[] b = new char[15];
-    	char[] c = new char[15];
-    	char[] d = new char[15];
-    	char[] e = new char[15];
+    	int[] arr = new int[31];
     	
-    	String a1 = br.readLine();
-    	String b1 = br.readLine();
-    	String c1 = br.readLine();
-    	String d1 = br.readLine();
-    	String e1 = br.readLine();
-    	
-    	for(int i=0;i<a1.length();i++) {
-    		a[i] = a1.charAt(i);
-    	}
-    	for(int i=0;i<b1.length();i++) {
-    		b[i] = b1.charAt(i);
-    	}
-    	for(int i=0;i<c1.length();i++) {
-    		c[i] = c1.charAt(i);
-    	}
-    	for(int i=0;i<d1.length();i++) {
-    		d[i] = d1.charAt(i);
-    	}
-    	for(int i=0;i<e1.length();i++) {
-    		e[i] = e1.charAt(i);
+    	for(int i=0;i<28;i++) {
+    		int n = Integer.parseInt(br.readLine());
+    		
+    		arr[n] = 1;
     	}
     	
-    	for(int i=0;i<15;i++) {
-    		sb.append(""+a[i]+b[i]+c[i]+d[i]+e[i]);
+    	for(int i=1;i<31;i++) {
+    		if(arr[i] == 0) {
+    			System.out.println(i);
+    		}
     	}
-    	
-    	System.out.println(sb.toString().replaceAll("\u0000",""));
     }
-
-
 }
