5dc1740903ae4cf4651f54b3ae181facd6c3c82f
mijosan <mijosan@naver.com>
2020-05-30 13:24:20 +0900

    구현-백준-10798번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -15,26 +15,42 @@ public class Main {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		// StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		// StringBuilder sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
     	
-    	int n = Integer.parseInt(br.readLine());
+    	char[] a = new char[15];
+    	char[] b = new char[15];
+    	char[] c = new char[15];
+    	char[] d = new char[15];
+    	char[] e = new char[15];
     	
-    	int count = 0;
+    	String a1 = br.readLine();
+    	String b1 = br.readLine();
+    	String c1 = br.readLine();
+    	String d1 = br.readLine();
+    	String e1 = br.readLine();
     	
-    	for(int i=0;i<n;i++) {
-    		int a = Integer.parseInt(br.readLine());
-    		
-    		if(a == 1) {
-    			count++;
-    		}
+    	for(int i=0;i<a1.length();i++) {
+    		a[i] = a1.charAt(i);
+    	}
+    	for(int i=0;i<b1.length();i++) {
+    		b[i] = b1.charAt(i);
+    	}
+    	for(int i=0;i<c1.length();i++) {
+    		c[i] = c1.charAt(i);
+    	}
+    	for(int i=0;i<d1.length();i++) {
+    		d[i] = d1.charAt(i);
+    	}
+    	for(int i=0;i<e1.length();i++) {
+    		e[i] = e1.charAt(i);
     	}
     	
-    	if(count > n/2) {
-    		System.out.println("Junhee is cute!");
-    	}else {
-    		System.out.println("Junhee is not cute!");
+    	for(int i=0;i<15;i++) {
+    		sb.append(""+a[i]+b[i]+c[i]+d[i]+e[i]);
     	}
+    	
+    	System.out.println(sb.toString().replaceAll("\u0000",""));
     }
 
 
