2e7fa62283ade6a42ac0632031317de1327c0b9f
mijosan <mijosan@naver.com>
2020-05-30 12:55:11 +0900

    구현-백준-5565번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -18,29 +18,17 @@ public class Main {
 		// StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
     	
-    	while(true) {
-    		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-    		
-    		int a = Integer.parseInt(st.nextToken());
-    		int b = Integer.parseInt(st.nextToken());
-    		int c = Integer.parseInt(st.nextToken());
-    		
-    		if(a == 0 && b == 0 && c == 0) {
-    			break;
-    		}
-    		
-    		if(a*a + b*b == c*c) {
-    			System.out.println("right");
-    		}else if(a*a + c*c == b*b) {
-    			System.out.println("right");
-    		}else if(c*c + b*b == a*a) {
-    			System.out.println("right");
-    		}else {
-    			System.out.println("wrong");
-    		}
+    	int n = Integer.parseInt(br.readLine());
+    	
+    	int sum = 0;
+    	for(int i=0;i<9;i++) {
+    		sum = sum + Integer.parseInt(br.readLine());
+    	}
+    	
+    	System.out.println(n-sum);
     		
     		
-    	}    	
+    	   	
     }
 
 
