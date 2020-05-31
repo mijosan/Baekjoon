9f8f700c4020e8286619e4e366685c2d857b6f92
mijosan <mijosan@naver.com>
2020-05-31 13:27:21 +0900

    구현-백준-2935번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,13 +1,7 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.math.BigInteger;
 
 public class Main {
 	
@@ -18,28 +12,14 @@ public class Main {
 		// StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
     	
-    	StringTokenizer st = new StringTokenizer(br.readLine()," ");
+    	BigInteger n = new BigInteger(br.readLine());
+        char c = br.readLine().charAt(0);
+        BigInteger m = new BigInteger(br.readLine());
+	
+        if(c == '*')
+            System.out.println(n.multiply(m));
+        else 
+            System.out.println(n.add(m));
     	
-    	int a = Integer.parseInt(st.nextToken());
-    	int b = Integer.parseInt(st.nextToken());
-    	int c = Integer.parseInt(st.nextToken());
-    	int d = Integer.parseInt(st.nextToken());
-    	
-    	int sum = a+b+c+d;
-    	
-    	st = new StringTokenizer(br.readLine()," ");
-    	
-    	int a1 = Integer.parseInt(st.nextToken());
-    	int b1 = Integer.parseInt(st.nextToken());
-    	int c1 = Integer.parseInt(st.nextToken());
-    	int d1 = Integer.parseInt(st.nextToken());
-    	
-    	int sum1 = a1+b1+c1+d1;
-    	
-    	if(sum>=sum1) {
-    		System.out.println(sum);
-    	}else {
-    		System.out.println(sum1);
-    	}
     }
 }
