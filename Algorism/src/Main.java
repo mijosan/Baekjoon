c52f632f541eae4e696ef55c3a9b4443744db16e
mijosan <mijosan@naver.com>
2020-06-03 10:25:55 +0900

    bfs-백준-2589번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -9,10 +9,11 @@ public class Main {
 	static int n;
 	static int m;
 	static char[][] map;
-	static int[][] count;
+	static int[][] countMap;
 	static int[][] visited;
 	static int[] x_move = {0, -1, 0, 1};
 	static int[] y_move = {-1, 0, 1, 0};
+	static int max;
 	
     public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,8 +27,6 @@ public class Main {
     	n = Integer.parseInt(st.nextToken());
     	m = Integer.parseInt(st.nextToken());
     	map = new char[n][m];
-    	count = new int[n][m];
-    	visited = new int[n][m];
     	
     	for(int i=0;i<n;i++) {
     		String temp = br.readLine();
@@ -42,78 +41,50 @@ public class Main {
     
     static void bfs() {
     	Queue<Pair> queue = new LinkedList<Pair>();
-    	boolean flag = false;
     	
     	for(int i=0;i<n;i++) {
     		for(int j=0;j<m;j++) {
-    			if(map[i][j] == '*') {
-    				queue.add(new Pair(i, j, '*'));
-    				visited[i][j] = 1;
-    			}
-    		}
-    	}
-    	
-    	for(int i=0;i<n;i++) {
-    		for(int j=0;j<m;j++) {
-    			if(map[i][j] == 'S') {
-    				queue.add(new Pair(i, j, 'S'));
-    				visited[i][j] = 1;
-    			}
-    		}
-    	}
-    	loop:
-    	while(!queue.isEmpty()) {
-    		Pair pair = queue.poll();
-    		
-    		int px = pair.i;
-    		int py = pair.j;
-    		char pc = pair.k;
-    		
-    		for(int i=0;i<4;i++) {
-    			int x = px + x_move[i];
-    			int y = py + y_move[i];
-    			
-    			if(x >= 0 && x < n && y >= 0 && y < m) {
-    				if(pc == '*') {
-	    				if(map[x][y] == '.' && visited[x][y] == 0) {
-	    					queue.add(new Pair(x, y, '*'));
-	    					map[x][y] = '*';
-	    					visited[x][y] = 1;	
-	    				}
-    				}
+    			if(map[i][j] == 'L') {
+    				visited = new int[n][m];
+    				countMap = new int[n][m];
     				
-    				if(pc == 'S') {
-	    				if(map[x][y] == '.' && visited[x][y] == 0) {
-	    					queue.add(new Pair(x, y, 'S'));
-	    					visited[x][y] = 1;
-	    					count[x][y] = count[px][py] + 1;    					
-	    				}
-	    				
-	    				if(map[x][y] == 'D') {
-	    					count[x][y] = count[px][py] + 1;
-	    					System.out.println(count[x][y]);
-	    					flag = true;
-	    					break loop;
-	    				}
+    				queue.add(new Pair(i, j));
+    				visited[i][j] = 1;
+    				
+    				while(!queue.isEmpty()) {
+    					Pair pair = queue.poll();
+    					
+    					int px = pair.i;
+    					int py = pair.j;
+    					
+    					for(int k=0;k<4;k++) {
+    						int x = px + x_move[k];
+    						int y = py + y_move[k];
+    						
+    						if(x >= 0 && x < n && y >=0 && y < m) {
+    							if(visited[x][y] == 0 && map[x][y] == 'L') {
+    								queue.add(new Pair(x, y));
+    								visited[x][y] = 1;
+    								countMap[x][y] = countMap[px][py] + 1;
+    								
+    								max = Math.max(max, countMap[x][y]);
+    							}
+    						}
+    					}
     				}
     			}
     		}
     	}
     	
-    	if(flag == false) {
-    		System.out.println("KAKTUS");
-    	}
+    	System.out.println(max);
     }
 }
 class Pair{
 	int i;
 	int j;
-	char k;
 	
-	Pair(int i, int j, char k){
+	Pair(int i, int j){
 		this.i = i;
 		this.j = j;
-		this.k = k;
 	}
 }
-
