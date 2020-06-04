d27776c7cf63ad6fdbb4b9363e89d74dfaede8c9
mijosan <mijosan@naver.com>
2020-06-04 23:33:18 +0900

    bfs-백준-2573번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -7,9 +7,15 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static boolean check;
-	static ArrayList<Integer>[] adjList;
-	static int[] visited;
+	static int n;
+	static int m;
+	static int[][] map;
+	static int[][] copyMap;
+	static int[][] visited;
+	static int[] x_move = {0, -1, 0, 1};
+	static int[] y_move = {-1, 0, 1, 0};
+	static int min;
+	static int count;
 	
     public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,65 +24,127 @@ public class Main {
 		// StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
     	
-    	int t = Integer.parseInt(br.readLine());
+    	StringTokenizer st = new StringTokenizer(br.readLine()," ");
     	
-    	for(int i=0;i<t;i++) {
-    		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+    	n = Integer.parseInt(st.nextToken());
+    	m = Integer.parseInt(st.nextToken());
+    	map = new int[n][m];
+    	copyMap = new int[n][m];
+    	
+    	for(int i=0;i<n;i++) {
+    		st = new StringTokenizer(br.readLine()," ");
     		
-    		int v = Integer.parseInt(st.nextToken());
-    		int e = Integer.parseInt(st.nextToken());
-    		
-    		adjList = new ArrayList[v + 1];
-    		visited = new int[v + 1];
-    		check = false;
-    		
-    		for(int j=1;j<adjList.length;j++) {
-    			adjList[j] = new ArrayList<Integer>();
+    		for(int j=0;j<m;j++) {
+    			int temp = Integer.parseInt(st.nextToken());
+    			map[i][j] = temp;
+    			copyMap[i][j] = temp;
     		}
-    		
-    		for(int j=0;j<e;j++) {
-    			st = new StringTokenizer(br.readLine()," ");
-    			
-    			int v1 = Integer.parseInt(st.nextToken());
-    			int v2 = Integer.parseInt(st.nextToken());
-    			
-    			adjList[v1].add(v2);
-    			adjList[v2].add(v1);
-    		}
-    		
-    		for (int j = 1; j < v + 1; j++) {
-				if(visited[j] == 0) {
-					bfs(j);
-				}
-			}
-    		
-    		if(check == true) {
-        		System.out.println("NO");
-        	}else {
-        		System.out.println("YES");
-        	}
-    	}	
-    }
-    static void bfs(int start){
-    	Queue<Integer> queue = new LinkedList<Integer>();
-    	queue.add(start);
-  
-    	visited[start] = 1;
+    	}
+    	boolean flag = false;
     	
     	loop:
-    	while(!queue.isEmpty()) {
-    		int v = queue.poll();
-    		
-    		for(int i : adjList[v]) {
-    			if(visited[i] == 0) {
-    				queue.add(i);
-    				visited[i] = visited[v] * -1;
- 
-    			}else if(visited[v] + visited[i] != 0) {
-    				check = true;
-    				break loop;
+    	for(int i=0;i<n;i++) {
+    		for(int j=0;j<m;j++) {
+    			if(map[i][j] != 0) {		
+    				bfs(i, j);
+    				count++;
+    			
+	    			min = 0;
+	    	    	checkBfs();
+	    	    	
+	    	    	if(min >= 2) {
+	    	    		System.out.println(count);
+	    	    		flag = true;
+	    	    		break loop;
+	    	    	}
     			}
     		}
-    	}     	
+    	}
+    	
+    	if(flag == false) {
+    		System.out.println(0);
+    	}
+    	
     }
+    
+    static void bfs(int i, int j) {
+    	Queue<Pair> queue = new LinkedList<Pair>();
+    	queue.add(new Pair(i, j));
+
+    	visited = new int[n][m];
+    	visited[i][j] = 1;
+    	
+    	while(!queue.isEmpty()) {
+    		Pair pair = queue.poll();
+    		
+    		int px = pair.i;
+    		int py = pair.j;
+    		
+    		for(int k=0;k<4;k++) {
+    			int x = px + x_move[k];
+    			int y = py + y_move[k];
+    			
+    			if(map[x][y] == 0) {
+    				copyMap[px][py]--;
+    				
+    				if(copyMap[px][py] < 0) {
+    					copyMap[px][py] = 0;
+    				}
+    				
+    			}else if(map[x][y] != 0 && visited[x][y] == 0) {
+    				queue.add(new Pair(x, y));
+    				visited[x][y] = 1;
+    			}
+    		}
+    	}
+    	
+    	for(int l=0;l<n;l++) {
+    		for(int o=0;o<m;o++) {
+    			map[l][o] = copyMap[l][o];
+    		}
+    	}
+    }
+    
+    static void checkBfs() {
+    	Queue<Pair> queue = new LinkedList<Pair>();
+    	visited = new int[n][m];
+    	
+    	for(int i=0;i<n;i++) {
+    		for(int j=0;j<m;j++) {
+    			if(map[i][j] != 0 && visited[i][j] == 0) {
+    				visited[i][j] = 1;
+    				queue.add(new Pair(i, j));
+    				
+    				while(!queue.isEmpty()) {
+    		    		Pair pair = queue.poll();
+    		    		
+    		    		int px = pair.i;
+    		    		int py = pair.j;
+    		    		
+    		    		for(int k=0;k<4;k++) {
+    		    			int x = px + x_move[k];
+    		    			int y = py + y_move[k];
+    		    			
+    		    			if(map[x][y] != 0 && visited[x][y] == 0) {
+    		    				queue.add(new Pair(x, y));
+    		    				visited[x][y] = 1;
+    		    			}
+    		    		}
+    		    	}
+    				
+    				min++;
+    			}
+    		}
+    	}
+    }
+}
+
+class Pair{
+	int i;
+	int j;
+	
+	Pair(int i, int j){
+		this.i = i;
+		this.j = j;
+	}
 }
