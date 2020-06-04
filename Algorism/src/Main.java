9b2c807eabb6c9764fd701d85a5481c55b25dad3
mijosan <mijosan@naver.com>
2020-06-04 13:59:45 +0900

    bfs-백준-1707번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,19 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
-	static int m;
-	static int[][] map;
-	static int[][] countMap;
-	static int[][][] visited;
-	static int[] x_move = {0, -1, 0, 1};
-	static int[] y_move = {-1, 0, 1, 0};
-	static int min = Integer.MAX_VALUE;
+	static boolean check;
+	static ArrayList<Integer>[] adjList;
+	static int[] visited;
 	
     public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,89 +18,65 @@ public class Main {
 		// StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
     	
-    	StringTokenizer st = new StringTokenizer(br.readLine()," ");
+    	int t = Integer.parseInt(br.readLine());
     	
-    	n = Integer.parseInt(st.nextToken());
-    	m = Integer.parseInt(st.nextToken());
-    	
-    	map = new int[n + 1][m + 1];
-    	countMap = new int[n + 1][m + 1];
-    	visited = new int[n + 1][m + 1][2];
-    	
-    	for(int i=1;i<=n;i++) {
-    		String temp = br.readLine();
-
-    		for(int j=1;j<=m;j++) {	 			
-    			map[i][j] = temp.charAt(j-1) - '0';
+    	for(int i=0;i<t;i++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+    		
+    		int v = Integer.parseInt(st.nextToken());
+    		int e = Integer.parseInt(st.nextToken());
+    		
+    		adjList = new ArrayList[v + 1];
+    		visited = new int[v + 1];
+    		check = false;
+    		
+    		for(int j=1;j<adjList.length;j++) {
+    			adjList[j] = new ArrayList<Integer>();
     		}
-    	}
-
-    	bfs(); 	
+    		
+    		for(int j=0;j<e;j++) {
+    			st = new StringTokenizer(br.readLine()," ");
+    			
+    			int v1 = Integer.parseInt(st.nextToken());
+    			int v2 = Integer.parseInt(st.nextToken());
+    			
+    			adjList[v1].add(v2);
+    			adjList[v2].add(v1);
+    		}
+    		
+    		for (int j = 1; j < v + 1; j++) {
+				if(visited[j] == 0) {
+					bfs(j);
+				}
+			}
+    		
+    		if(check == true) {
+        		System.out.println("NO");
+        	}else {
+        		System.out.println("YES");
+        	}
+    	}	
     }
-    
-    static void bfs() {
-    	Queue<Pair> queue = new LinkedList<Pair>();
-    	
-    	queue.add(new Pair(1, 1, 0));
-    	visited[1][1][0] = 1;
-    	boolean flag = false;
+    static void bfs(int start){
+    	Queue<Integer> queue = new LinkedList<Integer>();
+    	queue.add(start);
+  
+    	visited[start] = 1;
     	
     	loop:
     	while(!queue.isEmpty()) {
-    		Pair pair = queue.poll();
+    		int v = queue.poll();
     		
-    		int px = pair.i;
-    		int py = pair.j;
-    		int wall = pair.k;
-    		
-    		for(int i=0;i<4;i++) {
-    			int x = px + x_move[i];
-    			int y = py + y_move[i];	
-    			
-    			if(x >= 1 && x <= n && y >= 1 && y <= m) {
-    				if(wall == 0) {
-    					if(map[x][y] == 0 && visited[x][y][0] == 0) {			
-    						queue.add(new Pair(x, y, 0));
-    						visited[x][y][0] = 1;
-    						countMap[x][y] = countMap[px][py] + 1;
-    					}else if(map[x][y] == 1 && visited[x][y][0] == 0) {
-    						queue.add(new Pair(x, y, 1));
-    						visited[x][y][1] = 1;
-    						countMap[x][y] = countMap[px][py] + 1;
-    					}
-    					
-    				}else if(wall == 1) {
-    					if(map[x][y] == 0 && visited[x][y][1] == 0) {
-    						queue.add(new Pair(x, y, 1));
-    						visited[x][y][1] = 1;
-    						countMap[x][y] = countMap[px][py] + 1;
-    					}
-    				}
-    			}
-    			
-    			if(x == n && y == m) {
-    				flag = true;
+    		for(int i : adjList[v]) {
+    			if(visited[i] == 0) {
+    				queue.add(i);
+    				visited[i] = visited[v] * -1;
+ 
+    			}else if(visited[v] + visited[i] != 0) {
+    				check = true;
     				break loop;
     			}
     		}
-    	}
-
-    	if(flag == true || (n == 1 && m == 1)) {
-    		System.out.println(countMap[n][m] + 1);
-    	}else {
-    		System.out.println(-1);
-    	}
+    	}     	
     }
-
-}
-class Pair{
-	int i;
-	int j;
-	int k;
-	
-	Pair(int i, int j, int k){
-		this.i = i;
-		this.j = j;
-		this.k = k;
-	}
 }
