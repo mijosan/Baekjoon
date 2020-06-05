569b19bc732822c57bbfb99894694309af86a5e0
mijosan <mijosan@naver.com>
2020-06-05 13:43:05 +0900

    bfs-백준-9019번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -7,144 +7,98 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
-	static int m;
-	static int[][] map;
-	static int[][] copyMap;
-	static int[][] visited;
-	static int[] x_move = {0, -1, 0, 1};
-	static int[] y_move = {-1, 0, 1, 0};
-	static int min;
-	static int count;
-	
-    public static void main(String[] args) throws NumberFormatException, IOException {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		// StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		// StringBuilder sb = new StringBuilder();
 		// int n = Integer.parseInt(br.readLine());
     	
-    	StringTokenizer st = new StringTokenizer(br.readLine()," ");
+    	int t = Integer.parseInt(br.readLine());
     	
-    	n = Integer.parseInt(st.nextToken());
-    	m = Integer.parseInt(st.nextToken());
-    	map = new int[n][m];
-    	copyMap = new int[n][m];
-    	
-    	for(int i=0;i<n;i++) {
-    		st = new StringTokenizer(br.readLine()," ");
-    		
-    		for(int j=0;j<m;j++) {
-    			int temp = Integer.parseInt(st.nextToken());
-    			map[i][j] = temp;
-    			copyMap[i][j] = temp;
-    		}
-    	}
-    	boolean flag = false;
-    	
-    	loop:
-    	for(int i=0;i<n;i++) {
-    		for(int j=0;j<m;j++) {
-    			if(map[i][j] != 0) {		
-    				bfs(i, j);
-    				count++;
-    			
-	    			min = 0;
-	    	    	checkBfs();
-	    	    	
-	    	    	if(min >= 2) {
-	    	    		System.out.println(count);
-	    	    		flag = true;
-	    	    		break loop;
-	    	    	}
-    			}
-    		}
-    	}
-    	
-    	if(flag == false) {
-    		System.out.println(0);
+    	for(int i=0;i<t;i++) {
+	    	StringTokenizer st = new StringTokenizer(br.readLine()," ");
+	    	
+	    	int a = Integer.parseInt(st.nextToken());
+	    	int b = Integer.parseInt(st.nextToken());
+	    	
+	    	bfs(a, b);
     	}
     	
     }
     
-    static void bfs(int i, int j) {
+    static void bfs(int a, int b) {
     	Queue<Pair> queue = new LinkedList<Pair>();
-    	queue.add(new Pair(i, j));
-
-    	visited = new int[n][m];
-    	visited[i][j] = 1;
+    	int[] visited = new int[10000];
+    	
+    	queue.add(new Pair(a, ""));
+    	visited[a] = 1;
     	
     	while(!queue.isEmpty()) {
     		Pair pair = queue.poll();
     		
-    		int px = pair.i;
-    		int py = pair.j;
+    		int pa = pair.i;
+    		String cal = pair.j;
     		
-    		for(int k=0;k<4;k++) {
-    			int x = px + x_move[k];
-    			int y = py + y_move[k];
-    			
-    			if(map[x][y] == 0) {
-    				copyMap[px][py]--;
+    		visited[pa] = 1;
+    		
+    		if(pa == b) {
+    			System.out.println(cal);
+    			break;
+    		}
+    		
+    		for(int i=0;i<4;i++) {
+    			if(i == 0) {
+    				int result =  (2*pa) % 10000;
     				
-    				if(copyMap[px][py] < 0) {
-    					copyMap[px][py] = 0;
+    				if(result > 9999) {
+    					result = result % 10000;
     				}
     				
-    			}else if(map[x][y] != 0 && visited[x][y] == 0) {
-    				queue.add(new Pair(x, y));
-    				visited[x][y] = 1;
+    				if(result >= 0 && result < 10000 && visited[result] == 0) {
+    					visited[result] = 1;
+    					queue.add(new Pair(result, cal + "D"));		
+    				}
+    				
+    			}else if(i == 1) {
+    				int result = pa;
+    				
+    				if(result == 0) {
+    					result = 9999;
+    				}else {
+    					result = result - 1;
+    				}
+    				if(result >= 0 && result < 10000 && visited[result] == 0) {
+    					visited[result] = 1;
+    					queue.add(new Pair(result, cal + "S"));		
+    				}
+    				
+    			}else if(i == 2) {
+    				int result = (pa % 1000) * 10 + pa / 1000;
+    				
+    				if(result >= 0 && result < 10000 && visited[result] == 0) {
+    					visited[result] = 1;
+    					queue.add(new Pair(result, cal + "L"));		
+    				}
+    			}else if(i == 3){
+    				int result = (pa % 10) * 1000 + (pa / 10);
+    				
+    				if(result >= 0 && result < 10000 && visited[result] == 0) {
+    					visited[result] = 1;
+    					queue.add(new Pair(result, cal + "R"));		
+    				}
     			}
     		}
     	}
-    	
-    	for(int l=0;l<n;l++) {
-    		for(int o=0;o<m;o++) {
-    			map[l][o] = copyMap[l][o];
-    		}
-    	}
-    }
-    
-    static void checkBfs() {
-    	Queue<Pair> queue = new LinkedList<Pair>();
-    	visited = new int[n][m];
-    	
-    	for(int i=0;i<n;i++) {
-    		for(int j=0;j<m;j++) {
-    			if(map[i][j] != 0 && visited[i][j] == 0) {
-    				visited[i][j] = 1;
-    				queue.add(new Pair(i, j));
-    				
-    				while(!queue.isEmpty()) {
-    		    		Pair pair = queue.poll();
-    		    		
-    		    		int px = pair.i;
-    		    		int py = pair.j;
-    		    		
-    		    		for(int k=0;k<4;k++) {
-    		    			int x = px + x_move[k];
-    		    			int y = py + y_move[k];
-    		    			
-    		    			if(map[x][y] != 0 && visited[x][y] == 0) {
-    		    				queue.add(new Pair(x, y));
-    		    				visited[x][y] = 1;
-    		    			}
-    		    		}
-    		    	}
-    				
-    				min++;
-    			}
-    		}
-    	}
-    }
+    }  
 }
-
 class Pair{
 	int i;
-	int j;
+	String j = "";
 	
-	Pair(int i, int j){
+	Pair(int i, String j){
 		this.i = i;
-		this.j = j;
+		this.j = this.j + j;
 	}
 }
