49a1b5effc05dccdda1445b60cfa82c1eacb46e4
mijosan <mijosan@naver.com>
2020-06-07 15:29:41 +0900

    bfs-백준-11559번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -4,10 +4,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-	static char[][] map = new char[12][6];
+	static int[][] map = new int[12][6];
 	static int[][] visited;
 	static int[] x_move = {0, -1, 0, 1};
 	static int[] y_move = {-1, 0, 1, 0};
@@ -25,39 +24,35 @@ public class Main {
     		String temp = br.readLine();
     		
     		for(int j=0;j<6;j++) {
-    			map[i][j] = temp.charAt(j); 
+    			map[i][j] = temp.charAt(j);
     		}
     	}
     	
     	while(true) {
-    		boolean flag = false;
-    		visited = new int[12][6];
-    		
-    		for(int i=0;i<12;i++) {
-    			for(int j=0;j<6;j++) {
-    				if(visited[i][j] == 0 && map[i][j] != '.') {
-    					adjList = new ArrayList<Pair>();
-    					
-    					bfs(i, j);
-    					
-    					if(adjList.size() >= 4) {
-    						flag = true;
-    						
-    						for(Pair p : adjList) {
-    							map[p.i][p.j] = '.';
-    						}
-    					}				
-    				}
-    			}
-    		}
-    		
-    		if(flag == false) {
-    			break;
-    		}else {
-    			count++;
-    		}
-    		
-    		paint();
+	    	boolean flag = false;
+	    	
+	    	for(int i=0;i<12;i++) {	
+	    		for(int j=0;j<6;j++) {
+	    			if(map[i][j] != '.') {
+	    				bfs(i, j);
+	    				
+	    				if(adjList.size() >= 4) {
+	    					flag = true;
+	    					
+	    					for(Pair k : adjList) {
+	    						map[k.i][k.j] = '.';
+	    					}
+	    				}
+	    			}
+	    		}
+	    	}
+	    	
+	    	if(flag == true) {
+	    		count++;
+	    		paint();
+	    	}else {
+	    		break;
+	    	}
     	}
     	System.out.println(count);
     }
@@ -65,8 +60,12 @@ public class Main {
 	static void bfs(int i, int j) {
 		Queue<Pair> queue = new LinkedList<Pair>();
 		queue.add(new Pair(i, j));
-		visited[i][j] = 1;
+		
+		adjList = new ArrayList<Pair>();
 		adjList.add(new Pair(i, j));
+		
+		visited = new int[12][6];
+		visited[i][j] = 1;
 		
 		while(!queue.isEmpty()) {
 			Pair pair = queue.poll();
@@ -74,15 +73,15 @@ public class Main {
 			int px = pair.i;
 			int py = pair.j;
 			
-			for(int k=0;k<4;k++) {		
-				int x = pair.i + x_move[k];
-				int y = pair.j + y_move[k];
+			for(int k=0;k<4;k++) {
+				int x = px + x_move[k];
+				int y = py + y_move[k];
 				
 				if(x >= 0 && x < 12 && y >= 0 && y < 6) {
 					if(visited[x][y] == 0 && map[x][y] == map[px][py]) {
-						adjList.add(new Pair(x, y));
 						queue.add(new Pair(x, y));
 						visited[x][y] = 1;
+						adjList.add(new Pair(x, y));
 					}
 				}
 			}
@@ -90,20 +89,22 @@ public class Main {
 	}
 	
 	static void paint() {
-	    for (int i = 0; i < 6; i++) {
-	        for (int j = 10; j >= 0; j--) {
-	            for (int k = 11; k > j; k--) {
-	                if (map[j][i] != '.' && map[k][i] == '.') {
-	                    map[k][i] = map[j][i];
-	                    map[j][i] = '.';
-	                    break;
-	                }
-	            }
-	        }
-	    }
+		for(int i=0;i<6;i++) {
+			for(int j=11;j>0;j--) {
+				for(int k=j-1;k>-1;k--) {
+					if(map[j][i] == '.' && map[k][i] != '.') {
+						map[j][i] = map[k][i];
+						map[k][i] = '.';
+						
+						break;
+					}
+				}
+			}
+		}
 	}
- 
+	
 }
+
 class Pair{
 	int i;
 	int j;
