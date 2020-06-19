fab7d0f60a33a6e4d599e04209efb449b26c4300
mijosan <mijosan@naver.com>
2020-06-20 02:46:54 +0900

    BF-백준-14888번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,10 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	static int[] num;
+	static int[] op;
+	static int max = Integer.MIN_VALUE;
+	static int min = Integer.MAX_VALUE;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -13,62 +20,98 @@ public class Main {
     	// int n = Integer.parseInt(br.readLine());
     	
     	int n = Integer.parseInt(br.readLine());
-    	int[] arr = new int[n];
+    	num = new int[n];
+    	op = new int[4];
+
+    	StringTokenizer st = new StringTokenizer(br.readLine()," ");
     	
-    	for(int i=0;i<arr.length;i++) {
-    		arr[i] = i+1;
+    	for(int i=0;i<n;i++) {
+    		num[i] = Integer.parseInt(st.nextToken());
     	}
     	
-    	solution(arr);
+    	st = new StringTokenizer(br.readLine()," ");
+    	
+    	for(int i=0;i<4;i++) {
+    		op[i] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	bfs();
+    	
+    	System.out.println(max); 
+    	System.out.println(min);
+
 	}
 	
-	static void solution(int[] arr) {
-		while(true) {
-			for(int i=0;i<arr.length;i++) {
-				System.out.print(arr[i] + " ");
-			}
-			System.out.println();
+	static void bfs() {
+		Queue<Pair> queue = new LinkedList<Pair>();
+		queue.add(new Pair(num[0], op));
+		
+		for(int i=1;i<num.length;i++) {
 			
-			int a = arr.length-1;
+			int length = queue.size();
 			
-			boolean flag = false;
-			for(int i=a;i>0;i--) {	
-				if(arr[i] > arr[i-1]) {
-					a = i;
-					flag = true;
-					break;
+			for(int j=0;j<length;j++) {
+				Pair pair = queue.poll();
+				
+				int pn = pair.num;
+				int[] po = pair.op;
+						
+				if(po[0] != 0) {
+					int n = pn + num[i];
+					int[] o = po.clone();
+					o[0]--;
+
+					queue.add(new Pair(n, o));
+				}
+				
+				if(po[1] > 0) {
+					int n = pn - num[i];
+					int[] o = po.clone();
+					o[1]--;
+					
+					queue.add(new Pair(n, o));
+				}
+				
+				if(po[2] > 0) {
+					int n = pn * num[i];
+					int[] o = po.clone();
+					o[2]--;
+
+					queue.add(new Pair(n, o));
+				}
+				
+				if(po[3] > 0) {
+					int n = pn / num[i];
+					int[] o = po.clone();
+					o[3]--;
+					
+					queue.add(new Pair(n, o));
 				}
 			}
-			
-			if(flag == false) {
-				break;
-			}
-			
-			int b = arr.length-1;
-			
-			for(int i=b;i>=a;i--) {		
-				if(arr[i] > arr[a-1]) {
-					b = i;
-					break;
-				}
-			}
-			
-			int temp = arr[b];
-			arr[b] = arr[a-1];
-			arr[a-1] = temp;	
-			
-			int start = a;
-			int end = arr.length-1;
-	
-			for(int i=0;i<end-start;i++) {
-				for(int j=start;j<end-i;j++) {	
-					if(arr[j] > arr[j+1]) {
-						int temp2 = arr[j+1];
-						arr[j+1] = arr[j];
-						arr[j] = temp2;
-					}
-				}
-			}		
 		}
+		
+		while(!queue.isEmpty()) {
+			int temp = queue.poll().num;
+			
+			if(max < temp) {
+				max = temp;
+			}
+			
+			if(min > temp) {
+				min = temp;
+			}
+		}
+		
 	}
+	
 }
+class Pair { 
+	int num; 
+	int[] op; 
+	
+	Pair(int num, int[] op) { 
+		this.num = num; 
+		this.op = op; 
+	} 
+}
+
