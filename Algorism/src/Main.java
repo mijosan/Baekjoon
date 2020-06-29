19366d59f0061c9a86ab44454ed2246a6a3522a9
mijosan <mijosan@naver.com>
2020-06-29 14:15:12 +0900

    bfs-백준-6593번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -7,13 +7,17 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] x_move = {0, -1, 0, 1};
-	static int[] y_move = {-1, 0, 1, 0};
-	static char[][] map;
-	static int[][] countMap;
-	static int[][] visited;
-	static int w;
-	static int h;
+	static int[] x_move = {0, -1, 0, 1, 0, 0};
+	static int[] y_move = {-1, 0, 1, 0, 0, 0};
+	static int[] z_move = {0, 0, 0, 0, -1, 1};
+	
+	static char[][][] map;
+	static int[][][] countMap;
+	static int[][][] visited;
+	
+	static int l; //층
+	static int r; //행
+	static int c; //열
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,109 +26,95 @@ public class Main {
     	// StringBuilder sb = new StringBuilder();
     	// int n = Integer.parseInt(br.readLine());
     	
-    	int t = Integer.parseInt(br.readLine());
-    	
-    	for(int i=0;i<t;i++) {
-    		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-    		w = Integer.parseInt(st.nextToken());
-    		h = Integer.parseInt(st.nextToken());
-    		
-    		map = new char[h][w];
-    		countMap = new int[h][w];
-    		visited = new int[h][w];
-    		
-    		for(int j=0;j<h;j++) {
-    			String temp = br.readLine();
-    			
-    			for(int k=0;k<w;k++) {
-    				map[j][k] = temp.charAt(k);
-    			}
-    		}
-    		
-    		bfs();
+    	while(true) {
+	    	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    	
+	    	l = Integer.parseInt(st.nextToken());
+	    	r = Integer.parseInt(st.nextToken());
+	    	c = Integer.parseInt(st.nextToken());
+	    	
+	    	if(l == 0 && r == 0 && c == 0) break;
+	    	
+	    	map = new char[l][r][c];
+	    	visited = new int[l][r][c];
+	    	countMap = new int[l][r][c];
+	    	
+	    	for(int i=0;i<l;i++) {
+	    		for(int j=0;j<r;j++) {
+		    		String temp = br.readLine();
+		    		
+		    		for(int k=0;k<c;k++) {
+		    			map[i][j][k] = temp.charAt(k);
+		    		}
+	    		}
+	    		br.readLine();
+	    	}	    	
+	    	
+	    	bfs();
     	}
 	}
 	
 	static void bfs() {
 		Queue<Pair> queue = new LinkedList<Pair>();
 		boolean flag = false;
-		int count = 0;
 		
-		for(int i=0;i<h;i++) {
-			for(int j=0;j<w;j++) {
-				if(map[i][j] == '*') {
-					queue.add(new Pair(i, j, '*'));
-					visited[i][j] = 1;
+		loop1:
+		for(int i=0;i<l;i++) {
+			for(int j=0;j<r;j++) {
+				for(int k=0;k<c;k++) {
+					if(map[i][j][k] == 'S') {
+						queue.add(new Pair(i, j, k));
+						visited[i][j][k] = 1;
+						break loop1;
+					}
 				}
 			}
 		}
 		
-		for(int i=0;i<h;i++) {
-			for(int j=0;j<w;j++) {
-				if(map[i][j] == '@') {
-					queue.add(new Pair(i, j, '@'));
-					visited[i][j] = 1;
-				}
-			}
-		}
-		loop:
+		loop2:
 		while(!queue.isEmpty()) {
 			Pair pair = queue.poll();
 			
+			int pz = pair.z;
 			int px = pair.x;
 			int py = pair.y;
-			char type = pair.type;
-			
-			if(count == 0 && type == '@' && (px == 0 || py == 0 || px == h-1 || py == w-1)) {
-				 System.out.println(countMap[px][py] + 1);
-				 flag = true;
-				 break loop;
-			}
-			
-			for(int i=0;i<4;i++) {
+	
+			for(int i=0;i<6;i++) {
+				int z = pz + z_move[i];
 				int x = px + x_move[i];
 				int y = py + y_move[i];
 				
-				if(x >= 0 && y >= 0 && x < h && y < w) {
-					 if(type == '*' && (map[x][y] == '.' || map[x][y] == '@') && visited[x][y] == 0) {
-						 map[x][y] = type;
-						 queue.add(new Pair(x, y, type));
-						 visited[x][y] = 1;
-					 }
-					 
-					 if(type == '@' && map[x][y] == '.' && visited[x][y] == 0) { 
-						 map[x][y] = type;
-						 map[px][py] = '.';
-						 countMap[x][y] = countMap[px][py] + 1;
-						 queue.add(new Pair(x, y, type));
-						 visited[x][y] = 1;		
-						 count++;
-						 
-						 if(x == 0 || y == 0 || x == h-1 || y == w-1) {
-							 System.out.println(countMap[x][y] + 1);
-							 flag = true;
-							 break loop;
-						 }
-					 }
+				if(x >= 0 && y >= 0 && z >= 0 && x < r && y < c && z < l) {
+					if((map[z][x][y] == '.' || map[z][x][y] == 'E') && visited[z][x][y] == 0) {
+						visited[z][x][y] = 1;
+						countMap[z][x][y] = countMap[pz][px][py] + 1;
+						queue.add(new Pair(z, x, y));
+						
+						if(map[z][x][y] == 'E') {
+							System.out.println("Escaped in " + countMap[z][x][y] + " minute(s).");
+							flag = true;
+							
+							break loop2;
+						}
+					}
 				}
 			}
 		}
 		
 		if(flag == false) {
-			System.out.println("IMPOSSIBLE");
+			System.out.println("Trapped!");
 		}
-
 	}
-	
 }
+
 class Pair{
+	int z;
 	int x;
 	int y;
-	char type;
 	
-	Pair(int x, int y, char type){
+	Pair(int z, int x, int y){
+		this.z = z;
 		this.x = x;
 		this.y = y;
-		this.type = type;
 	}
 }
