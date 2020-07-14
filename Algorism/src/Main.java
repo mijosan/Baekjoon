7b07091d67ee616fcb46f6c999ec5e8d1d3da46b
ChoiTaesan <ChoiTaesan@DESKTOP-G9QT27C>
2020-07-14 23:03:22 +0900

    3184번 푸는중

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,23 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] x_move = {0, -1, 0, 1, 0, 0};
-	static int[] y_move = {-1, 0, 1, 0, 0, 0};
-	static int[] z_move = {0, 0, 0, 0, -1, 1};
+	static char[][] map;
+	static boolean[][] visited;
 	
-	static char[][][] map;
-	static int[][][] countMap;
-	static int[][][] visited;
+	static int[] x_move = {0, -1, 0, 1};
+	static int[] y_move = {-1, 0, 1, 0};
 	
-	static int l; //층
-	static int r; //행
-	static int c; //열
+	static int r;
+	static int c;
+	
+	static int sheepCount;
+	static int wolfCount;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,94 +25,63 @@ public class Main {
     	// StringBuilder sb = new StringBuilder();
     	// int n = Integer.parseInt(br.readLine());
     	
-    	while(true) {
-	    	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-	    	
-	    	l = Integer.parseInt(st.nextToken());
-	    	r = Integer.parseInt(st.nextToken());
-	    	c = Integer.parseInt(st.nextToken());
-	    	
-	    	if(l == 0 && r == 0 && c == 0) break;
-	    	
-	    	map = new char[l][r][c];
-	    	visited = new int[l][r][c];
-	    	countMap = new int[l][r][c];
-	    	
-	    	for(int i=0;i<l;i++) {
-	    		for(int j=0;j<r;j++) {
-		    		String temp = br.readLine();
-		    		
-		    		for(int k=0;k<c;k++) {
-		    			map[i][j][k] = temp.charAt(k);
-		    		}
-	    		}
-	    		br.readLine();
-	    	}	    	
-	    	
-	    	bfs();
+    	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    	
+    	r = Integer.parseInt(st.nextToken());
+    	c = Integer.parseInt(st.nextToken());
+    	
+    	map = new char[r][c];
+    	visited = new boolean[r][c];
+    	
+    	for(int i = 0; i < r; i++) {
+    		String temp = br.readLine();
+    		
+    		for(int j = 0; j < c; j++) {
+    			map[i][j] = temp.charAt(j);
+    		}
     	}
+    	
+    	bfs();
 	}
 	
 	static void bfs() {
 		Queue<Pair> queue = new LinkedList<Pair>();
-		boolean flag = false;
 		
-		loop1:
-		for(int i=0;i<l;i++) {
-			for(int j=0;j<r;j++) {
-				for(int k=0;k<c;k++) {
-					if(map[i][j][k] == 'S') {
-						queue.add(new Pair(i, j, k));
-						visited[i][j][k] = 1;
-						break loop1;
-					}
-				}
-			}
-		}
-		
-		loop2:
-		while(!queue.isEmpty()) {
-			Pair pair = queue.poll();
-			
-			int pz = pair.z;
-			int px = pair.x;
-			int py = pair.y;
-	
-			for(int i=0;i<6;i++) {
-				int z = pz + z_move[i];
-				int x = px + x_move[i];
-				int y = py + y_move[i];
-				
-				if(x >= 0 && y >= 0 && z >= 0 && x < r && y < c && z < l) {
-					if((map[z][x][y] == '.' || map[z][x][y] == 'E') && visited[z][x][y] == 0) {
-						visited[z][x][y] = 1;
-						countMap[z][x][y] = countMap[pz][px][py] + 1;
-						queue.add(new Pair(z, x, y));
+		for(int i = 0; i < r; i++) {
+			for(int j = 0; j < c; j++) {
+				if(visited[i][j] == false && map[i][j] == 'v' || map[i][j] == 'o') { //한 영역에 들어갔다는뜻
+					queue.add(new Pair(i, j));
+					visited[i][j] = true;
+					
+					int landSheepCount = 0;
+					int landWolfCount = 0;
+					
+					while(!queue.isEmpty()) {
+						Pair pair = queue.poll();
 						
-						if(map[z][x][y] == 'E') {
-							System.out.println("Escaped in " + countMap[z][x][y] + " minute(s).");
-							flag = true;
+						int px = pair.x;
+						int py = pair.y;
+						
+						for(int k = 0; k < 4; k++) {
+							int x = px + x_move[k];
+							int y = py + y_move[k];
 							
-							break loop2;
+							if(x >= 0 && y >= 0 && x < r && y < c) {
+								
+							}
 						}
 					}
 				}
 			}
 		}
-		
-		if(flag == false) {
-			System.out.println("Trapped!");
-		}
 	}
 }
 
 class Pair{
-	int z;
 	int x;
 	int y;
 	
-	Pair(int z, int x, int y){
-		this.z = z;
+	Pair(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
