7ed0c93f4f394b333c0d44926d69ba05167dfe86
ChoiTaesan <ChoiTaesan@DESKTOP-G9QT27C>
2020-07-20 19:45:22 +0900

    구현-백준-2783번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -7,14 +7,7 @@ import java.util.StringTokenizer;
 
 
 public class Main {
-    
-	static int[][] map;
-	static int[][] visited;
-	static int n;
-	
-	static int[] x_move = {0, -1, 0, 1};
-	static int[] y_move = {-1, 0, 1, 0};
-	
+
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -22,67 +15,28 @@ public class Main {
         // StringBuilder sb = new StringBuilder();
         // int n = Integer.parseInt(br.readLine());
         
-        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
-        map = new int[n][n];
+        int price = Integer.parseInt(st.nextToken());
+        int gram = Integer.parseInt(st.nextToken());
+        
+        int n  = Integer.parseInt(br.readLine());
+        
+        double min = (double)price / gram * 1000;
         
         for(int i = 0; i < n; i++) {
-        	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        	st = new StringTokenizer(br.readLine(), " ");
         	
-        	for(int j = 0; j < n; j++) {
-        		map[i][j] = Integer.parseInt(st.nextToken());
-        	}
+        	int price2 = Integer.parseInt(st.nextToken());
+            int gram2 = Integer.parseInt(st.nextToken());
+            
+            double result = (double)price2 / gram2 * 1000;
+            
+            if(min > result) {
+            	min = result;
+            }
         }
         
-        bfs();
-    }
-    
-    static void bfs() {
-    	Queue<Pair> queue = new LinkedList<Pair>();
-    	visited = new int[n][n];
-    	
-    	int label = 1;
-    	
-    	for(int i = 0; i < n; i++) {
-    		for(int j = 0; j < n; j++) {
-    			if(visited[i][j] == 0 && map[i][j] == 1) {
-    				queue.add(new Pair(i, j));
-    				visited[i][j] = 1;
-    				map[i][j] = label;
-    				
-    				while(!queue.isEmpty()) {
-    					Pair pair = queue.poll();
-    					
-    					int px = pair.x;
-    					int py = pair.y;
-    					
-    					for(int k = 0; k < 4; k++) {
-    						int x = px + x_move[k];
-    						int y = py + y_move[k];
-    						
-    						if(x >= 0 && y >= 0 && x < n && y < n) {
-    							if(visited[x][y] == 0 && map[x][y] == 1) {
-    								queue.add(new Pair(x, y));
-    								visited[x][y] = 1;
-    								map[x][y] = label;
-    							}
-    						}
-    					}
-    				}
-    				
-    				label++;
-    			}
-    		}
-    	}
-    }
-}
-
-class Pair{
-    int x;
-    int y;
-    
-    Pair(int x, int y){
-        this.x = x;
-        this.y = y;
+        System.out.println(min);
     }
 }
